ec68a27fdd4cdfd382801d912c2427d41ea9db24
APOKALYPTOS <240228627+apokalyptos3@users.noreply.github.com>
2025-10-28 17:39:12 +0100

    Delete apokalyptos-empire-main directory

--- apokalyptos-empire-main/backend/src/routes/atopia.ts
+++
@@ -1,4 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-// TODO: Implement atopia routes
-export default router;
--- apokalyptos-empire-main/backend/src/routes/budget.ts
+++
@@ -1,4 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-// TODO: Implement budget routes
-export default router;
